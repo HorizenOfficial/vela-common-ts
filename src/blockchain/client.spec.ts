3f8e17bb6739144ec7bf1e19b198ceb680af79ab
ANDREA NISTICO <nistic.andrea@gmail.com>
2026-01-22 15:57:24 +0100

    almost finished test

--- src/blockchain/client.spec.ts
+++ src/blockchain/client.spec.ts
@@ -2,13 +2,18 @@ import { JsonRpcProvider } from "ethers";
 import ganache, { Server } from "ganache";
 import assert from "assert";
 import { HorizenPESClient, RequestType } from "../blockchain/client.js";
-import { AuthorityRegistry__factory, MockTeeAuthenticator__factory, ProcessorEndpoint__factory } from "../typechain-types/index.js";
+import { AuthorityRegistry, AuthorityRegistry__factory, MockTeeAuthenticator__factory, ProcessorEndpoint, ProcessorEndpoint__factory } from "../typechain-types/index.js";
+import { encrypt, exportPublicKeyToHex, P521KeyPair } from "../crypto/p521.js";
+import { deriveP521PrivateKeyFromSigner, deriveP521PrivateKeyFromSignerWithCustomChallenge } from "../crypto/wallet.js";
+import { bytesToString, stringToBytes } from "../crypto/utils.js";
 
 let server: Server;
 let provider: JsonRpcProvider;
 let client: HorizenPESClient;
+let processorEndpoint: ProcessorEndpoint;
+let authorityRegistry: AuthorityRegistry;
+let teePubSecp: P521KeyPair;
 
-const TEST_PUBSECP = "0x1234"
 const NODE_PORT = 9545
 
 describe("Client test", function () {
@@ -23,13 +28,19 @@ describe("Client test", function () {
     provider = new JsonRpcProvider(`http://127.0.0.1:${NODE_PORT}`);
     await provider.getBlockNumber();
 
+    //generate key
+
     //deploy contracts
     const signer = await provider.getSigner(0);
-    const authorityRegistry = await new AuthorityRegistry__factory(signer).deploy(signer, signer);
-    await authorityRegistry.waitForDeployment();
-    const teeAuthenticator = await new MockTeeAuthenticator__factory(signer).deploy(signer, TEST_PUBSECP);
+    teePubSecp = await deriveP521PrivateKeyFromSignerWithCustomChallenge(signer, "tee secp challenge", true);
+    const pubKey = await exportPublicKeyToHex(teePubSecp.publicKey);
+
+    const teeAuthenticator = await new MockTeeAuthenticator__factory(signer).deploy(signer, "0x" + pubKey);
     await teeAuthenticator.waitForDeployment();
-    const processorEndpoint = await new ProcessorEndpoint__factory(signer).deploy(teeAuthenticator, authorityRegistry, signer, signer, 0);
+    
+    authorityRegistry = await new AuthorityRegistry__factory(signer).deploy(signer, signer);
+    await authorityRegistry.waitForDeployment();
+    processorEndpoint = await new ProcessorEndpoint__factory(signer).deploy(teeAuthenticator, authorityRegistry, signer, signer, 0);
     await processorEndpoint.waitForDeployment();
 
     client = new HorizenPESClient(signer, true, await authorityRegistry.getAddress(), await teeAuthenticator.getAddress(), await processorEndpoint.getAddress());
@@ -48,18 +59,33 @@ describe("Client test", function () {
 
   it("getPublicKey", async () => {
     const publicKey = await client.getTeePublicKey();
-    assert.equal(publicKey, TEST_PUBSECP);
+    const expectedPubKey = await exportPublicKeyToHex(teePubSecp.publicKey);
+    assert.equal(publicKey, "0x"+expectedPubKey);
   })
 
   it("submitRequest", async () => {
-    const submitRequestTx = await client.submitRequest(0, 1, RequestType.PROCESS, Uint8Array.from([1, 2, 3]), BigInt(10), BigInt(0));
-    assert.notEqual(submitRequestTx, undefined);
-    assert.notEqual(submitRequestTx.hash, undefined);
-
-    const receipt = await submitRequestTx.wait();
-    assert.notEqual(receipt, undefined);
+    const submitResponse = await client.submitRequestAndWaitForRequestId(0, 1, RequestType.PROCESS, Uint8Array.from([1, 2, 3]), BigInt(10), BigInt(0));
+    assert.notEqual(submitResponse.requestId, undefined);
+    assert.notEqual(submitResponse.transactionReceipt, undefined);
   })
 
+  it("getCurrentUserEvents (just decrypting - without retrieving events)", async () => {
+    //submit request
+    const submitResponse = await client.submitRequestAndWaitForRequestId(0, 1, RequestType.PROCESS, Uint8Array.from([1, 2, 3]), BigInt(10), BigInt(0));
+    assert.notEqual(submitResponse.requestId, undefined);
+
+    //generate user key
+    const userKeyPair = await deriveP521PrivateKeyFromSigner(await provider.getSigner(0), true);
+    //encrypt event
+    const eventMessage = "Hello world!";
+    const encrypted = await encrypt(teePubSecp.privateKey, userKeyPair.publicKey, stringToBytes(eventMessage));
+
+    //retrieve and decrypt event
+    const events = await client.decryptAndFilterEvents([encrypted], undefined, (event: Uint8Array) => true, true);
+    assert.equal(events.length, 1);
+    const decryptedMessage = bytesToString(events[0]);
+    assert.equal(decryptedMessage, eventMessage);
+  });
 
 
 });
--- src/blockchain/client.ts
+++ src/blockchain/client.ts
@@ -1,8 +1,8 @@
-import { ContractTransactionResponse } from "ethers";
+import { ContractTransactionReceipt, ContractTransactionResponse } from "ethers";
 import { Signer } from "ethers";
 import { ITeeAuthenticator, ITeeAuthenticator__factory, ProcessorEndpoint, ProcessorEndpoint__factory } from "../typechain-types";
 import { deriveP521PrivateKeyFromSigner } from "../crypto/wallet";
-import { decrypt, importPublicKeyFromHex } from "../crypto/p521";
+import { decrypt, importPublicKeyFromHex, P521KeyPair } from "../crypto/p521";
 import { stringToBytes } from "../crypto/utils";
 
 export enum RequestType {
@@ -10,6 +10,13 @@ export enum RequestType {
   PROCESS, 
   DEANONYMIZATION, 
   ASSOCIATEKEY
+}
+
+export class RequestReceipt {
+  constructor(
+    public requestId: string, 
+    public transactionReceipt: ContractTransactionReceipt
+  ) {}
 }
 
 export class HorizenPESClient {
@@ -26,6 +33,10 @@ export class HorizenPESClient {
     
   }
 
+  async getSignerKeyPair(): Promise<P521KeyPair> {
+    return await deriveP521PrivateKeyFromSigner(this.signer, this.useAlternativeSign);
+  }
+
   async submitRequest(protocolVersion: number, applicationId: number, requestType: RequestType, payload: Uint8Array, depositAmount: bigint, maxFeeValue: bigint): Promise<ContractTransactionResponse> {
     const tx = await this.processorEndpoint.submitRequest(
       protocolVersion,
@@ -37,6 +48,18 @@ export class HorizenPESClient {
       {value: depositAmount}
     );
     return tx;
+  }
+
+  async submitRequestAndWaitForRequestId(protocolVersion: number, applicationId: number, requestType: RequestType, payload: Uint8Array, depositAmount: bigint, maxFeeValue: bigint): Promise<RequestReceipt> {
+    const tx = await this.submitRequest(protocolVersion, applicationId, requestType, payload, depositAmount, maxFeeValue);
+    //get request id from event
+    const receipt = await tx.wait();
+    const events = receipt?.logs;
+    const requestId = events && events.length > 0 ? events[0].topics[1] : undefined;
+    if(!requestId) {
+      throw new Error("Request ID not found in transaction events");
+    }
+    return new RequestReceipt(requestId, receipt!);
   }
 
   async getTeePublicKey(): Promise<string> {
@@ -57,12 +80,6 @@ export class HorizenPESClient {
       throw new Error("fromBlock cannot be less than toBlock");
     }
 
-    //recover decrypt key
-    const teePublicKeyString = await this.getTeePublicKey();
-    const teePublicKey = await importPublicKeyFromHex(teePublicKeyString);
-
-    const privateKey = await deriveP521PrivateKeyFromSigner(this.signer, this.useAlternativeSign);
-
     //get UserEvent events from Processor Endpoint
     const events = await this.processorEndpoint.queryFilter(
       this.processorEndpoint.filters.UserEvent(eventSubType),
@@ -70,11 +87,20 @@ export class HorizenPESClient {
       fromBlock
     );
 
+    return await this.decryptAndFilterEvents(events.map(e => stringToBytes(e.args.encryptedData)), eventSubType, filter, stopAtFirst);
+  }
+
+  async decryptAndFilterEvents(encryptedDatas: Uint8Array[], eventSubType: string | undefined,filter: (event: Uint8Array) => boolean, stopAtFirst: boolean): Promise<Uint8Array[]> {
+    //recover decrypt key
+    const teePublicKeyString = await this.getTeePublicKey();
+    const teePublicKey = await importPublicKeyFromHex(teePublicKeyString);
+    const privateKey = (await this.getSignerKeyPair()).privateKey;
+
     //decrypt and filter
     const returnEvents: Uint8Array[] = [];
-    for(let i = events.length - 1; i >= 0; i--) {
-      const event = events[i];
-      const decryptedData: Uint8Array = await decrypt(privateKey.privateKey, teePublicKey, stringToBytes(event.args.encryptedData));
+    for(let i = encryptedDatas.length - 1; i >= 0; i--) {
+      const event = encryptedDatas[i];
+      const decryptedData: Uint8Array = await decrypt(privateKey, teePublicKey, stringToBytes(event.toString()));
       if (filter(decryptedData)) {
         returnEvents.push(decryptedData);
       }
