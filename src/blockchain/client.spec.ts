313d28be85f64e15dfe91a6c0c27b7dae2aa0717
ANDREA NISTICO <nistic.andrea@gmail.com>
2026-01-26 10:55:54 +0100

    fix typo

--- src/blockchain/client.spec.ts
+++ src/blockchain/client.spec.ts
@@ -75,18 +75,18 @@ describe("CCE Client test", function () {
     //encrypt event
     const eventMessage = "Hello world!";
     const correctEvent = await encrypt(teePubSecp.privateKey, userKeyPair.publicKey, stringToBytes(eventMessage));
-    const uncorrectEvent = await encrypt(teePubSecp.privateKey, otherUserKeyPair.publicKey, stringToBytes(eventMessage));
+    const incorrectEvent = await encrypt(teePubSecp.privateKey, otherUserKeyPair.publicKey, stringToBytes(eventMessage));
 
     //retrieve and decrypt event
-    let events = await client.decryptAndFilterEvents([uncorrectEvent, correctEvent, correctEvent], undefined, (event: Uint8Array) => true, true); //stop at first
+    let events = await client.decryptAndFilterEvents([incorrectEvent, correctEvent, correctEvent], undefined, (event: Uint8Array) => true, true); //stop at first
     assert.equal(events.length, 1);
     const decryptedMessage = bytesToString(events[0]);
     assert.equal(decryptedMessage, eventMessage);
 
-    events = await client.decryptAndFilterEvents([uncorrectEvent], undefined, (event: Uint8Array) => true, true); // only uncorrect events
+    events = await client.decryptAndFilterEvents([incorrectEvent], undefined, (event: Uint8Array) => true, true); // only incorrect events
     assert.equal(events.length, 0);
 
-    events = await client.decryptAndFilterEvents([uncorrectEvent, correctEvent, correctEvent], undefined, (event: Uint8Array) => true, false); //not stop at first
+    events = await client.decryptAndFilterEvents([incorrectEvent, correctEvent, correctEvent], undefined, (event: Uint8Array) => true, false); //not stop at first
     assert.equal(events.length, 2);
   });
 
